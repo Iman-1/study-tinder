7b5d0e5b97f3c5132abacff6be626aacd2b6363e
Jmk2684 <jasmehar.kaur@mail.utoronto.ca>
2025-10-25 19:31:44 -0400

    colors

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -44,11 +44,11 @@ const Sidebar = () => {
 						) : (
 							matches.map((match) => (
 								<Link key={match._id} to={`/chat/${match._id}`}>
-									<div className='flex items-center mb-4 cursor-pointer hover:bg-pink-50 p-2 rounded-lg transition-colors duration-300'>
+									<div className='flex items-center mb-4 cursor-pointer hover:bg-[#e8ab92] p-2 rounded-lg transition-colors duration-300'>
 										<img
 											src={match.image || "/avatar.png"}
 											alt='User avatar'
-											className='size-12 object-cover rounded-full mr-3 border-2 border-pink-300'
+											className='size-12 object-cover rounded-full mr-3 border-2 border-[#825644]'
 										/>
 
 										<h3 className='font-semibold text-gray-800'>{match.name}</h3>
@@ -61,7 +61,7 @@ const Sidebar = () => {
 			</div>
 
 			<button
-				className='lg:hidden fixed top-4 left-4 p-2 bg-pink-500 text-white rounded-md z-0'
+				className='lg:hidden fixed top-4 left-4 p-2 bg-[#825644] text-white rounded-md z-0'
 				onClick={toggleSidebar}
 			>
 				<MessageCircle size={24} />
