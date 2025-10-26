42b224b86c6423cf49258ac5d82c5aa43ea9f100
Jmk2684 <jasmehar.kaur@mail.utoronto.ca>
2025-10-25 20:30:30 -0400

    brown smiley

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -55,7 +55,7 @@ export default HomePage;
 
 const NoMoreProfiles = () => (
 	<div className='flex flex-col items-center justify-center h-full text-center p-8'>
-		<SmileIcon className='text-pink-400 mb-6' size={80} />
+		<SmileIcon className='text-[#825644] mb-6' size={80} />
 		<h2 className='text-3xl font-bold text-gray-800 mb-4'>Woah there, speedy fingers!</h2>
 		<p className='text-xl text-gray-600 mb-6'> Matches will come soon!</p>
 	</div>
@@ -68,7 +68,7 @@ const LoadingUI = () => {
 				<div className='px-4 pt-4 h-3/4'>
 					<div className='w-full h-full bg-gray-200 rounded-lg' />
 				</div>
-				<div className='card-body bg-gradient-to-b from-white to-pink-50 p-4'>
+				<div className='card-body bg-gradient-to-b from-white to-[#825644] p-4'>
 					<div className='space-y-2'>
 						<div className='h-6 bg-gray-200 rounded w-3/4' />
 						<div className='h-4 bg-gray-200 rounded w-1/2' />
